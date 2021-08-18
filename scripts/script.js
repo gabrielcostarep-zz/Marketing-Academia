37b5bc28e70d65b4e1cca352e065d09630bbee7b
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-08-18 03:36:31 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -69,7 +69,7 @@ function contact() {
             <div class="location">
                 <h1>Onde nos encontrar</h1>
                 <div id="gps">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.8920011787673!2d-46.62644258502187!3d-23.572321984677405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce597067308c15%3A0x4e362f968369a397!2sAv.%20Lacerda%20Franco%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1629145964512!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.8920011787673!2d-46.62644258502187!3d-23.572321984677405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce597067308c15%3A0x4e362f968369a397!2sAv.%20Lacerda%20Franco%2C%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1629145964512!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="auto"></iframe>
                 <div>
             </div>
         </section>`
